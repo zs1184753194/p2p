9b6ab3ede6a5adace86a328f75a05f3e37af39df
zhangshuai <zs1184753194@163.com>
2019-06-26 11:52:08 +0800

    添加src的java代码

---
+++ src/main/java/com/bw/P2pPortalApplication.java
@@ -0,0 +1,13 @@
+package com.bw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2pPortalApplication.class, args);
+	}
+
+}
